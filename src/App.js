ab4680d5ab0523e9841b647111a4fc3a422179ee
viktoriia-yash <yashkina.vic@gmail.com>
2022-09-27 16:00:18 +0200

    I integrated API into this project

--- src/App.js
+++ src/App.js
@@ -1,91 +1,116 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
-export default function App() {
-  return (
-    <div className="page">
-      <div className="weather-container">
-        <div className="searchThings">
-          <form id="search">
-            <input
-              type="text"
-              placeholder="Enter a city"
-              autoFocus="on"
-              className="searchBar"
-            />
-            <input type="button" value="Search" className="searchButton" />
-            <input
-              type="button"
-              value="Current location"
-              className="locationButton"
-            />
-          </form>
-        </div>
+export default function App(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-        <div className="cityAndDate">
-          <span>
-            <h1 className="cityName">Oslo</h1>
-          </span>
-          <div id="date">
-            <h3>Sunday 19:00</h3>
-          </div>
-        </div>
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      date: "Sunday 09:00",
+      iconUrl: "https://openweathermap.org/img/wn/10d@2x.png",
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
 
-        <div className="weatherAllInformationt">
-          <span id="temperatureToday">
-            <span id="icon-container">
-              <img
-                src="https://openweathermap.org/img/wn/10d@2x.png"
-                alt="icon weather"
-                id="icon-pic"
+  if (weatherData.ready) {
+    return (
+      <div className="page">
+        <div className="weather-container">
+          <div className="searchThings">
+            <form id="search">
+              <input
+                type="text"
+                placeholder="Enter a city"
+                autoFocus="on"
+                className="searchBar"
               />
-            </span>
-            <span id="temperatureNumber">23</span>
-            <span id="FahPlusCel">
-              <span id="celsius-un">
-                <a href="/" id="celsius">
-                  °C
-                </a>{" "}
-              </span>
-              <span id="separator">|</span>
-              <span id="fahrenheit-un">
-                {" "}
-                <a href="/" id="fahrenheit">
-                  °F
-                </a>
-              </span>
-            </span>
-          </span>
+              <input type="button" value="Search" className="searchButton" />
+              <input
+                type="button"
+                value="Current location"
+                className="locationButton"
+              />
+            </form>
+          </div>
 
-          <span className="weatherDesc">
-            <ul>
-              <li>
-                <span id="description"></span>
-              </li>
-              <li>
-                Humidity: <span id="humidity"></span> %
-              </li>
-              <li>
-                Wind: <span id="wind"></span> km/h
-              </li>
-            </ul>
-          </span>
+          <div className="cityAndDate">
+            <span>
+              <h1 className="cityName">{weatherData.city}</h1>
+            </span>
+            <div id="date">
+              <h3>{weatherData.date}</h3>
+            </div>
+          </div>
+
+          <div className="weatherAllInformationt">
+            <span id="temperatureToday">
+              <span id="icon-container">
+                <img
+                  src={weatherData.iconUrl}
+                  alt={weatherData.description}
+                  id="icon-pic"
+                />
+              </span>
+              <span id="temperatureNumber">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span id="FahPlusCel">
+                <span id="celsius-un">
+                  <a href="/" id="celsius">
+                    °C
+                  </a>{" "}
+                </span>
+                <span id="separator">|</span>
+                <span id="fahrenheit-un">
+                  {" "}
+                  <a href="/" id="fahrenheit">
+                    °F
+                  </a>
+                </span>
+              </span>
+            </span>
+
+            <span className="weatherDesc">
+              <ul>
+                <li>
+                  <span id="description">{weatherData.description}</span>
+                </li>
+                <li>
+                  Humidity: <span id="humidity">{weatherData.humidity}</span> %
+                </li>
+                <li>
+                  Wind: <span id="wind">{weatherData.wind}</span> km/h
+                </li>
+              </ul>
+            </span>
+          </div>
+
+          <hr />
         </div>
-
-        <hr />
-
-        <div className="weatherForecast" id="forecast"></div>
+        <p className="credit">
+          <a
+            href="https://github.com/viktoriia-yash/weather-app-react"
+            className="github"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Viktoriia Yashkina
+        </p>
       </div>
-      <p className="credit">
-        <a
-          href="https://github.com/viktoriia-yash/weather-app-react"
-          className="github"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by Viktoriia Yashkina
-      </p>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "1766cbbe43ef71cdc8ece5c867ddb581";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
